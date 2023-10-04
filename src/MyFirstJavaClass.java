46834e7af00a156ffca57513bd3222693fe70e12
Aria <aria.daneshmand@dci-student.org>
2023-10-04 11:46:54 +0200

    added branches

---
+++ src/MyFirstJavaClass.java
@@ -0,0 +1,13 @@
+public class MyFirstJavaClass {
+
+    // This is a single-line comment
+
+    /*
+    This is a
+    multiline comment
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello my name is Aria and i am 31 years old"); // Print some text
+    }
+}
